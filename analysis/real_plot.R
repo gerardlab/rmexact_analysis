9f80bd0685d048b8ba40b2602532849434bfaa14
Karene <70621033+Karene123@users.noreply.github.com>
2023-02-14 15:25:57 -0500

    fixed an error

--- analysis/real_plot.R
+++ analysis/real_plot.R
@@ -5,7 +5,6 @@ library(tidyr)
 
 realdata <- readRDS("./output/sturg/sims_realdata.RDS")
 
-
 #Exact P-values
 realdata %>%
   ggplot(mapping = aes(sample = exact_pval)) +
@@ -53,18 +52,6 @@ realdata %>%
   xlab("Theoretical Quantiles") +
   ylab("Observed P-values") -> splitlrtplotrealdata
 
-realdata %>%
-  ggplot(mapping = aes(sample = splitlrt_pval)) +
-  geom_qq(distribution = stats::qunif) +
-  facet_grid()
-  geom_abline() +
-  ggtitle("Split Likelihood P-value") +
-  theme_bw() +
-  theme(strip.background = element_rect(fill = "white")) +
-  xlab("Theoretical Quantiles") +
-  ylab("Observed P-values")
-  
-
 ##All QQplots 
 extractedexactpval <- realdata$exact_pval 
 extractedchisqrpval <- realdata$chisqr_pval
@@ -93,19 +80,3 @@ combinedtab %>%
    ylab("Observed P-values") -> realdataplots
 
 ggsave(filename = "./output/sturg/realdataplots.pdf", plot = realdataplots, height = 6, width = 6, family = "Times")
-
-#Likelihood Pvalue
-paramdf0 %>%
-  dplyr::mutate(p2 = as.character(p)) %>%
-  ggplot2::ggplot(mapping = aes(sample = like_pval))+
-  ggplot2::geom_qq(distribution = stats::qunif) +
-  ggplot2::facet_grid(p2 ~ n) +
-  ggplot2::geom_abline() +
-  ggplot2::ggtitle("Likelihood P-value under Alternative Hypothesis") +
-  theme_bw() +
-  theme(strip.background = element_rect(fill = "white")) +
-  coord_cartesian(xlim = c(0.0001, 1), ylim = c(0.0001, 1)) +
-  xlab("Theoretical Quantiles") +
-  ylab("Observed P-values") -> lrtplot2
-
-ggsave(filename = "./output/sturg/splitlrtdata.pdf", plot = splitlrtplotrealdata, height = 6, width = 6, family = "Times")
