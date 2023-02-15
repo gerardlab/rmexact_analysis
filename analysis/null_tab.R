68d2f32e4915cfdcd592ee14046d906f23424ee1
David Gerard <gerard.1787@gmail.com>
2023-02-15 11:53:14 -0500

    reorder of table

--- analysis/null_tab.R
+++ analysis/null_tab.R
@@ -18,12 +18,13 @@ paramdf %>%
   pivot_longer(cols = c("Chi-squared", "LRT", "Exact", "SLRT"),
                names_to = "Method",
                values_to = "pval") %>%
+  mutate(Method = parse_factor(Method, levels = c("Chi-squared", "LRT", "Exact", "SLRT"))) %>%
   group_by(p, n, Method) %>%
   summarize(t1e = mean(pval < alpha)) %>%
   ungroup() %>%
   pivot_wider(names_from = Method, values_from = t1e) %>%
   xtable(digits = c(0, 0, 0, rep(2, 4)),
          label = "tab:t1e",
-         caption = "Type I error at significance level $\\alpha = 0.01$ for four tests considered in this thesis for different gamete frequencies and different sample sizes. Since the null is satisfied, all values should be at or below 0.01.") %>%
+         caption = "Type I error at significance level $\\alpha = 0.01$ for the four tests considered in this thesis for different gamete frequencies and different sample sizes. Since the null is satisfied, all values should be at or below 0.01.") %>%
   print(include.rownames = FALSE) %>%
   writeLines(con = "./output/null/null_tab.txt")
